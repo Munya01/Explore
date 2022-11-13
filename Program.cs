3efd5e8bfb877056bb8ca64b18a623ac2e870864
Munya01 <91569375+Munya01@users.noreply.github.com>
2022-11-13 15:17:13 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,41 +16,25 @@ class GlazerCalc
 
         string widthString, heightString;
 
-        Console.Write("Give the width of the window: ");
-        widthString = Console.ReadLine();
-        width = double.Parse(widthString);
 
-        if (width < MIN_WIDTH)
+     
+
+        do
         {
-            Console.WriteLine("Width is too small.\n\n");
-            Console.WriteLine("Using minimum");
-            width = MIN_WIDTH;
-        }
+            Console.WriteLine("Give the width of the window betweeen" + MIN_WIDTH + " and " + MAX_WIDTH);
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+        } while (width < MIN_WIDTH || width > MAX_WIDTH);
 
-        if (width > MAX_WIDTH)
+
+        do
         {
-            Console.WriteLine("Width is too large.\n\n");
-            Console.WriteLine("Using maximum");
-            width = MAX_WIDTH;
-        }
+            Console.WriteLine("Give the height of the window betweeen" + MIN_HEIGHT + " and " + MAX_HEIGHT);
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+        } while (height < MIN_HEIGHT || height > MAX_HEIGHT);
 
-        Console.Write("Give the height of the window: ");
-        heightString = Console.ReadLine();
-        height = double.Parse(heightString);
-
-        if (height < MIN_HEIGHT)
-        {
-            Console.WriteLine("Height is too small.\n\n");
-            Console.WriteLine("Using minimum");
-            height = MIN_HEIGHT;
-        }
-
-        if (height > MAX_HEIGHT)
-        {
-            Console.WriteLine("Height is too large.\n\n");
-            Console.WriteLine("Using maximum");
-            height = MAX_HEIGHT;
-        }
+      
 
 
         woodLength = 2 * (width + height) + 3.25;
